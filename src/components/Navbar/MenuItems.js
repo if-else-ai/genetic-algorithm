ab99a67525bf459dab5219dd9b4ae398038cb40c
Zebra <Thirawatp9@gmail.com>
2021-09-11 01:19:03 +0700

    ✨ navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "About",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Genetic Generator",
+    url: "#",
+    cName: "nav-links",
+  },
+];
